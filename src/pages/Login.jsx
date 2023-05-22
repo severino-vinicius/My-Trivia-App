d9bf09ebf3c8db96172e7189925695898d6e5c7c
Severino Vinicius <severino.sales95@gmail.com>
2023-05-22 17:37:43 -0300

    [req - 02] em desenvolvimento e com erros

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import getTokenApi from '../services/tokenApi';
 
 class Login extends Component {
   state = {
@@ -26,6 +28,13 @@ class Login extends Component {
     }, this.verifyInput);
   };
 
+  handleLoginClick = async () => {
+    const { history } = this.props;
+    const resultTokenApi = await getTokenApi();
+    localStorage.setItem('token', JSON.stringify(resultTokenApi));
+    history.push('/Game');
+  };
+
   render() {
     const { email, userName, saveBtnDisabbled } = this.state;
     return (
@@ -49,7 +58,7 @@ class Login extends Component {
         <button
           type="button"
           disabled={ saveBtnDisabbled }
-          // onClick={  }
+          onClick={ this.handleLoginClick }
           data-testid="btn-play"
         >
           Play
@@ -58,5 +67,11 @@ class Login extends Component {
     );
   }
 }
+
+Login.propTypes = {
+  history: PropTypes.shape({
+    push: PropTypes.func,
+  }).isRequired,
+};
 
 export default Login;
