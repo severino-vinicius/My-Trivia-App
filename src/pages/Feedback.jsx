cbcbaabc03e53d2068215c40d804a09d946ae85a
Victorio Peagno <vpn.cash@gmail.com>
2023-05-24 17:28:03 -0300

    [Req 13] adiciona texto se acertou mais ou menos de 3 perguntas lendo do estado global o numero de acertos

--- src/pages/Feedback.jsx
+++ src/pages/Feedback.jsx
@@ -1,14 +1,29 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 import Header from '../components/Header';
 
 class Feedback extends Component {
   render() {
+    const { assertionsRedux } = this.props;
+    const verifyScore = 3;
     return (
       <section>
         <Header />
+        <h3 data-testid="feedback-text">
+          { assertionsRedux >= verifyScore ? 'Well Done!' : 'Could be better...' }
+        </h3>
       </section>
     );
   }
 }
 
-export default Feedback;
+Feedback.propTypes = {
+  assertionsRedux: PropTypes.number,
+}.isRequired;
+
+const mapStateToProps = (state) => ({
+  assertionsRedux: state.player.assertions,
+});
+
+export default connect(mapStateToProps)(Feedback);
