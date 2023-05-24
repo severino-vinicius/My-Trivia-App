5ae6cecb700866f09deb0b6f2920a7f76450e1b8
Victorio Peagno <vpn.cash@gmail.com>
2023-05-24 17:25:07 -0300

    [Req 12] adiciona Header a pagina Feedback

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import './App.css';
 import Login from './pages/Login';
 import Game from './pages/Game';
 import Settings from './pages/Settings';
+import Feedback from './pages/Feedback';
 
 export default function App() {
   return (
@@ -11,6 +12,7 @@ export default function App() {
       <Route exact path="/" component={ Login } />
       <Route path="/Game" component={ Game } />
       <Route path="/Settings" component={ Settings } />
+      <Route path="/Feedback" component={ Feedback } />
     </Switch>
   );
 }
